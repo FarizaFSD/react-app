ea97077bc259c854d34fd8b41e2c8f3ddd621a43
FARİZA <ahmetova.fariza@gmail.com>
2023-09-17 19:49:10 +0300

    add cart,CartContext,CartReducer

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -6,13 +6,15 @@ const MealItemForm = (props) => {
       <Input
         label="Amount"
         input={{
-          id: "amount",
+          id: "amount_" + props.id,
           type: "number",
           min: "1",
           max: "5",
+          step: "1",
           defaultValue: "1",
         }}
       />
+
       <button>+ Add</button>
     </form>
   );
